eeacf226a64022538aaf3837fbe6c7c9e5b44e84
evgeny_ivashov <eugeny.ivashov.webdev@yandex.ru>
2025-10-25 14:22:13 +0700

    Initial commit

---
+++ src/ui/BaseInput.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+interface BaseInputProps {
+  type?: 'text' | 'password' | 'email' | 'password_confirmation'
+}
+
+defineProps<{ msg: string }>()
+</script>
+
+<template>
+  <input type="text">
+</template>
+
+<style scoped>
+</style>
